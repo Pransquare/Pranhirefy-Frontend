43b9be6216c23b7345592c1274b8beca6e280544
Pransquare <chagamobulesu@pransquare.in>
2025-05-22 19:33:51 +0530

    just solved the error

--- src/services/LeaveTypeService.js
+++ src/services/LeaveTypeService.js
@@ -1,119 +1,34 @@
-const BASE_URL = 'http://localhost:8080/api/leavetypes';
+const BASE_URL = "http://localhost:8080/api/leavetypes";
 
-
+// GET all leave types
 export const getLeaveTypes = async () => {
-  const res = await fetch(BASE_URL, {
-   
-  });
+  const res = await fetch(BASE_URL);
   return res.json();
 };
 
+// POST: create a leave type
 export const createLeaveType = async (leaveType) => {
   const res = await fetch(BASE_URL, {
-    method: 'POST',
-  
-    body: JSON.stringify(leaveType)
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(leaveType),
   });
   return res.json();
 };
 
+// PUT: update a leave type
 export const updateLeaveType = async (id, leaveType) => {
   const res = await fetch(`${BASE_URL}/${id}`, {
-    method: 'PUT',
-    
-    body: JSON.stringify(leaveType)
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(leaveType),
   });
   return res.json();
 };
 
+// DELETE: delete a leave type
 export const deleteLeaveType = async (id) => {
   await fetch(`${BASE_URL}/${id}`, {
-    method: 'DELETE',
-
+    method: "DELETE",
   });
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const BASE_URL = 'http://localhost:8080/api/leavetypes';
- 
-// export const getLeaveTypes = async () => {
-//   const res = await fetch(BASE_URL);
-//   return res.json();
-// };
- 
-// export const createLeaveType = async (leaveType) => {
-//   const res = await fetch(BASE_URL, {
-//     method: 'POST',
-//     headers: { 'Content-Type': 'application/json' },
-//     body: JSON.stringify(leaveType)
-//   });
-//   return res.json();
-// };
- 
-// export const updateLeaveType = async (id, leaveType) => {
-//   const res = await fetch(`${BASE_URL}/${id}`, {
-//     method: 'PUT',
-//     headers: { 'Content-Type': 'application/json' },
-//     body: JSON.stringify(leaveType)
-//   });
-//   return res.json();
-// };
- 
-// export const deleteLeaveType = async (id) => {
-//   await fetch(`${BASE_URL}/${id}`, { method: 'DELETE' });
-// };
- 
- 
